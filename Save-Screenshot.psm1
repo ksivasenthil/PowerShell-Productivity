f76e05b5b7ba041d07813bb53cb3d868f35ce399
K. Siva Senthil <ksivasenthil@users.noreply.github.com>
2018-04-11 21:05:20 +0530

    Content and productivity tool added
    Content for ReadMe.md
    Save-Screenshot productivity tool.

---
+++ Save-Screenshot.psm1
@@ -0,0 +1,12 @@
+﻿Function Save-Screenshot() {
+    $directoryExists = Test-Path -Path "$($env:USERPROFILE)\Screenshots";
+    if(-not($directoryExists)){
+        New-Item -Path "$($env:USERPROFILE)\Screenshots\" -ItemType Directory;    }
+    $screenShot = Get-Clipboard -Format Image;
+    if($null -ne $screenShot) {
+        $fileName = Get-Date -Format "dd-MM-yyyy-HH-mm-ss-fff";
+        $fileName = "$($env:USERPROFILE)\Screenshots\$fileName.png"
+        $screenShot.Save($fileName, "png");
+    }
+}
+Export-ModuleMember -Function Save-Screenshot
