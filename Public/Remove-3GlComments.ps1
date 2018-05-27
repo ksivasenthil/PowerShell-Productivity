28e64b323027efbe12411c011ccc77eada0def9e
K. Siva Senthil <ksivasenthil@users.noreply.github.com>
2018-05-27 22:37:03 +0530

    Added scaffolding code for the Cmdlet which will remove comments from code files written using 3-generation language

---
+++ Public/Remove-3GlComments.ps1
@@ -0,0 +1,23 @@
+<#
+    .SYNOPSIS
+    .DESCRIPTION
+    .PARAMETER
+    .EXAMPLE
+    .NOTE
+#>
+Function Remove-3GlComments {
+    [CmdletBinding()]
+    Param(
+        [Switch]
+        $SingleLine,
+        
+        [String]
+        $Path,
+
+        [String]
+        $CommentToken
+    )
+    Process{
+        
+    }
+}
