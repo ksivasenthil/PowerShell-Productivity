e98305e6e5f28bc945186e50dd5eb899f83b84dd
K. Siva Senthil <ksivasenthil@users.noreply.github.com>
2018-05-27 20:06:52 +0530

    Added documentation text and code for Get-FileNamesByExtension Cmdlet

--- Public/Get-FileNamesByExtension.ps1
+++ Public/Get-FileNamesByExtension.ps1
@@ -1,9 +1,29 @@
 <#
     .SYNOPSIS
+    Lists all the files with a specific extension in the path you asked the command to search inside.
     .DESCRIPTION
-    .PARAMETER
+    File extensions that this Cmdlet can operate is limited to cs, vb, py, and csproj. 
+    The reason for this limitation is the subsequent use of which for the time being will be built only for those file types.
+    .PARAMETER Extension
+    Optional in nature, file extension which you want to be listed. It should one of the cs, vb, py, or csproj. Defaults to cs.
+    Only one file extension can be used with the Cmdlet.
+    .PARAMETER OutputPropertyName
+    Optional in nature, you specify the name of the property which you will get as output from this Cmdlet. 
+    The custom property name will be applied to the PSCustomObject emitted from this Cmdlet.
+    This will help in situations where you intend to pipeline this Cmdlet to other Cmdlets down in the chain.
+    .PARAMETER Recurse
+    Optional in nature, it is a switch indicating whether you intend to recurse through sub-folders present in the current directory.
+    .PARAMETER Path
+    Required parameter, it should be the fully qualified or relative path within which the files will be searched.
+    In scenarios where Recurse switch is present it will recurse all sub-folders present inside this folder.
     .EXAMPLE
+    Get-FileNamesByExtension -Path "C:\Source code" -Extension "py"
+    Path
+    -----
+    [List of files with .py extension as directy child to the current folder.]
     .NOTE
+    This Cmdlet has the process block and can operate through multiple Path and Extension parameters.
+    The Cmdlet is pipeline friendly for downstream Cmdlets as well.
 #>
 Function Get-FileNamesByExtension {
     [CmdletBinding()]
@@ -11,7 +31,8 @@ Function Get-FileNamesByExtension {
         [Parameter(
             Mandatory=$false,
             ValueFromPipelineByPropertyName=$true,
-            HelpMessage="Give one of the extension string which will be used to search. Extensions are limited to cs, vb, py, csproj. Defaults to cs"
+            HelpMessage="Give one of the extension string which will be used to search. `
+                        Extensions are limited to cs, vb, py, csproj. Defaults to cs"
         )]
         [ValidateSet("cs", "vb", "py", "csproj")]
         [Alias("ex")]
@@ -21,7 +42,8 @@ Function Get-FileNamesByExtension {
         [Parameter(
             Mandatory=$false,
             ValueFromPipelineByPropertyName=$false,
-            HelpMessage="Give the property name of the PSCustomObject which be emitted as output of the Cmdlet. Use only string, refrain from using numbers and special characters.Defaults to Path"
+            HelpMessage="Give the property name of the PSCustomObject which be emitted as output of the Cmdlet. `
+                        Use only string, refrain from using numbers and special characters.Defaults to Path"
         )]
         [ValidateNotNullOrEmpty()]
         [Alias("opn")]
@@ -52,6 +74,7 @@ Function Get-FileNamesByExtension {
         $Path
     )
     Process{
-        Write-Output (Get-ChildItem -Path $Path -Filter "*.$Extension" -Recurse:$Recurse | Select-Object -Property @{n=$OutputPropertyName;e={$_.FullName}});
+        Write-Output (Get-ChildItem -Path $Path -Filter "*.$Extension" -Recurse:$Recurse |`
+                         Select-Object -Property @{n=$OutputPropertyName;e={$_.FullName}});
     }
 }
