caa26f32467806e9160c067536949f0089a74729
K. Siva Senthil <ksivasenthil@users.noreply.github.com>
2018-06-10 18:59:16 +0530

    Checking in code scaffolding for Get-ContentAsObject.

---
+++ Public/Get-ContentAsObject.ps1
@@ -0,0 +1,20 @@
+﻿<#
+    .SYNOPSIS
+    .DESCRIPTION
+    .PARAMETER 1
+    .PARAMETER 2
+    .PARAMETER 3
+    .PARAMETER 4
+    .PARAMETER 5
+    .EXAMPLE
+    .NOTES
+#>
+Function Get-ContentAsObject {
+    [CmdletBinding()]
+    Param(
+
+    )
+    Process {
+
+    }
+}
